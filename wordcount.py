c0573487d15d50465a9b9ac9579333f684db575c
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-27 20:15:30 -0700

    Replace if-logic with .get-logic

--- wordcount.py
+++ wordcount.py
@@ -1,16 +1,13 @@
 # put your code here.
-my_file = open("test.txt","r")
+my_file = open("twain.txt","r")
 word_frequency = {}
 
 # my_lines = my_file.rstrip("\n").readlines()
 for line in my_file:
 	line_list = line.strip("\n").split(" ")
 	for word in line_list:
-		if word in word_frequency:
-			word_frequency[word] +=1
-		else:
-			word_frequency[word] = 1
-			
+		word_frequency[word] = word_frequency.get(word, 0) + 1
+
 for key_word, count in word_frequency.items():
 	print("{} {}".format(key_word,count))
 	
